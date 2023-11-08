ea3c570283493c5bbf440fb82527ca8ad7054273
weirui88888 <ruiwei88888@163.com>
2023-11-08 15:21:57 +0800

    46

--- b.js
+++ b.js
@@ -1,1 +1,1 @@
-const adb = () => console.log('add b1.js')
+const adb = () => console.log('add b1.js1')
--- server/main.js
+++ server/main.js
@@ -1,1 +1,1 @@
-const minus = (x, y) => x - y - 3 - 3 - 1 + 3 - 2
+const minus = (x, y) => x - y - 3 - 3 - 1 + 3 - 2 - 3
