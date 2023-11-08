41aff1c774071b6b37e6a1845542eeaea8e6c835
weirui88888 <ruiwei88888@163.com>
2023-11-08 13:55:44 +0800

    22

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-const add = (x, y) => x + y + 3 + 4 + 1
+const add = (x, y) => x + y + 3 + 4 + 1 + 1
--- server/main.js
+++ server/main.js
@@ -1,1 +1,1 @@
-const minus = (x, y) => x - y - 3 - 3 - 1
+const minus = (x, y) => x - y - 3 - 3 - 1 + 3
